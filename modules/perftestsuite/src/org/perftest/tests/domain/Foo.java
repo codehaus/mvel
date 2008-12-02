8cc4fbd30963784352f8a8e88b570f3b808d5c4a
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2008-12-02 21:51:33 +0000

    parser improvements

---
+++ modules/perftestsuite/src/org/perftest/tests/domain/Foo.java
@@ -0,0 +1,13 @@
+package org.perftest.tests.domain;
+
+public class Foo {
+    private Bar bar;
+
+    public Bar getBar() {
+        return bar;
+    }
+
+    public void setBar(Bar bar) {
+        this.bar = bar;
+    }
+}
