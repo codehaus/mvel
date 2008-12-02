27a621a8bf162837482229c6f27172e8a0f32610
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2008-12-02 21:51:30 +0000

    parser improvements

---
+++ modules/perftestsuite/src/org/perftest/tests/domain/Bar.java
@@ -0,0 +1,14 @@
+package org.perftest.tests.domain;
+
+
+public class Bar {
+    private String name = "Mr. Bar";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
