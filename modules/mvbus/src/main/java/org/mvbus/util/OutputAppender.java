341b5c785d7b7b08009088ec61f92c3a605e555a
mbrock <mbrock@9462aea2-c925-0410-9681-c018ebb51820>
2009-04-07 00:13:13 +0000

    refactorings...

--- modules/mvbus/src/main/java/org/mvbus/util/OutputAppender.java
+++ modules/mvbus/src/main/java/org/mvbus/util/OutputAppender.java
@@ -1,6 +1,9 @@
 package org.mvbus.util;
 
 
+/**
+ * A simple interface to create an abstract layer between the engine and the output method.
+ */
 public interface OutputAppender {
     public OutputAppender append(String str);
 }
